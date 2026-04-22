c02ad75212da4c248ba4e95f88c6f77d0677d06a
Gert Lindström <32777118+gertlind@users.noreply.github.com>
2026-04-22 17:52:43 +0200

    Obfuscate TAG_MAC for security reasons

--- nfc_to_epaper.py
+++ nfc_to_epaper.py
@@ -10,7 +10,7 @@ import datetime
 API_BASE = "http://localhost:3456/api"
 
 OPENEPAPER_HOST = "192.168.123.90"
-TAG_MAC = "00EE6933F1335F55"
+TAG_MAC = "00EE6XXXXXXXXX"
 
 GET_UID = [0xFF, 0xCA, 0x00, 0x00, 0x00]
 MIME = b"application/vnd.openprinttag"
